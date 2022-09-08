ff9322ba6957e79d2a8c0ce900faa07da9e98302
vinyashegde <vinyasvasanthegde7@gmail.com>
2022-09-09 02:39:25 +0530

    commit

---
+++ Nakuls Youtube/background.js
@@ -0,0 +1,25 @@
+// Called when the user clicks on the browser action icon.
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+        //var new_url = tab.url.replace(tab.url, "https://www.yout-ube.com/watch?v="+tab.url);
+        //var new_url = tab.url.replace(tab.url, "https://www.yout-ube.com/watch?v="+tab.url);
+        //var tablink = tab.url.length;
+        //console.log(tablink);
+        //chrome.tabs.update(tab.id, { url: new_url });
+
+        var a = tab.url;
+        var b = "-";
+        var position = 16;
+        var output = [a.slice(0, position), b, a.slice(position)].join('');
+
+        console.log(output); //debug
+
+        chrome.tabs.update(tab.id, { url: output });
+        setTimeout(myFunction, 1000);
+        //chrome.tabs.reload();
+
+});
+
+function myFunction() {
+        chrome.tabs.reload();
+      }
